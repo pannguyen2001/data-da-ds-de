d2512b03f8aeda10a9fb2a25b27499824c8e18d1
anotherone2xxx <anotherone2xxx@gmail.com>
2026-03-13 12:54:47 +0000

    update code

--- data_validation_pandas/helpers/check_mandatory.py
+++ data_validation_pandas/helpers/check_mandatory.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from typing import Tuple, List
 from .is_empty import is_empty
 from utils.constant import error_message
@@ -7,8 +8,11 @@ from .validation_wrapper import validation_wrapper
 # --------- Check mandatory ----------
 
 @validation_wrapper
-def check_mandatory(df: pd.DataFrame = None, column_name: str = "", empty_value_list: List = None) -> Tuple[pd.Series, str]:
+def check_mandatory(df: pd.DataFrame = None, column_name: str = "", empty_value_list: List = ["", None, "nan", "null", "NaN", np.nan, pd.NA]) -> Tuple[pd.Series, str]:
     """Return True if value cell is empty else False."""
+    # if df is None or df.empty:
+    #     logger.error("Dataframe is empty or having error.")
+    #     return df
 
     message: str = error_message["check_mandatory"].format(column_name)
 
